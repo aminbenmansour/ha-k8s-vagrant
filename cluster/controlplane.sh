46d11ce020cad8d4f813f011b2728e1b9ff86bf3
aminbenmansour <amin.benmansour.10@gmail.com>
2024-10-09 23:30:42 +0200

    feat: generate a cluster-join script

--- cluster/controlplane.sh
+++ cluster/controlplane.sh
@@ -3,8 +3,23 @@
 POD_CIDR=10.244.0.0/16
 SERVICE_CIDR=10.96.0.0/16
 
-kubeadm init --pod-network-cidr $POD_CIDR --service-cidr $SERVICE_CIDR --apiserver-advertise-address $PRIMARY_IP
 
+# initialize the controlplane node
+JOIN_COMMAND=$(kubeadm init --pod-network-cidr $POD_CIDR --service-cidr $SERVICE_CIDR --apiserver-advertise-address $PRIMARY_IP | tail -n 2)
+
+
+# deploy pod network to the cluster
 kubectl --kubeconfig /etc/kubernetes/admin.conf \
     apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
 
+
+# generate the join command for worker nodes
+cat <<EOF > worker-node.sh
+#!/bin/bash
+
+$JOIN_COMMAND
+EOF
+
+
+# protect script against write access
+chmod 551 ./worker-node.sh
